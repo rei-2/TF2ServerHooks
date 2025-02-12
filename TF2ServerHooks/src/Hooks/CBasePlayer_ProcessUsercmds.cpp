bb2894370ad15306faae6482b979907fbedde45d
rei-2 <174158077+rei-2@users.noreply.github.com>
2025-02-12 00:06:12 -0500

    print weapon timings while reloading rather than when in fire

--- TF2ServerHooks/src/Hooks/CBasePlayer_ProcessUsercmds.cpp
+++ TF2ServerHooks/src/Hooks/CBasePlayer_ProcessUsercmds.cpp
@@ -17,12 +17,12 @@ MAKE_HOOK(CBasePlayer_ProcessUsercmds, S::CBasePlayer_ProcessUsercmds(), void,
 	{
 		//SDK::Output("ProcessUsercmds", std::format("{}: {}", SDK::PlatFloatTime(), std::bit_cast<int32_t>(float(SDK::PlatFloatTime() * 1000.0)) & 255).c_str(), { 255, 0, 0, 255 });
 
-		auto pPlayer = reinterpret_cast<CTFPlayer*>(rcx);
+		//auto pPlayer = reinterpret_cast<CTFPlayer*>(rcx);
 
-		float flTime = TICKS_TO_TIME(pPlayer->m_nTickBase());
-		float flAttack = pPlayer->m_flNextAttack();
-		SDK::Output("ProcessUsercmds", std::format("{}, {}, {}", flTime - flAttack, TIME_TO_TICKS(flAttack - flTime), flTime < flAttack).c_str());
-		SDK::OutputClient("ProcessUsercmds", std::format("{}, {}, {}", flTime - flAttack, TIME_TO_TICKS(flAttack - flTime), flTime < flAttack).c_str(), pPlayer);
+		//float flTime = TICKS_TO_TIME(pPlayer->m_nTickBase());
+		//float flAttack = pPlayer->m_flNextAttack();
+		//SDK::Output("ProcessUsercmds", std::format("{}, {}, {}", flTime - flAttack, TIME_TO_TICKS(flAttack - flTime), flTime < flAttack).c_str());
+		//SDK::OutputClient("ProcessUsercmds", std::format("{}, {}, {}", flTime - flAttack, TIME_TO_TICKS(flAttack - flTime), flTime < flAttack).c_str(), pPlayer);
 	}
 
 	if (G::ServerHitboxes)
@@ -44,4 +44,24 @@ MAKE_HOOK(CBasePlayer_ProcessUsercmds, S::CBasePlayer_ProcessUsercmds(), void,
 	}
 
 	return CALL_ORIGINAL(rcx, cmds, numcmds, totalcmds, dropped_packets, paused);
+}
+
+MAKE_SIGNATURE(CTFWeaponBase_IncrementAmmo, "server.dll", "48 89 5C 24 ? 57 48 83 EC ? 48 8B D9 E8 ? ? ? ? 48 8B C8 C7 44 24 ? ? ? ? ? 4C 8D 0D ? ? ? ? 33 D2 4C 8D 05 ? ? ? ? E8 ? ? ? ? 80 BB", 0x0);
+
+MAKE_HOOK(CTFWeaponBase_IncrementAmmo, S::CTFWeaponBase_IncrementAmmo(), void,
+	void* rcx)
+{
+	auto pWeapon = reinterpret_cast<CTFWeaponBase*>(rcx);
+	auto pLocal = pWeapon->m_hOwnerEntity().Get()->As<CTFPlayer>();
+	if (pLocal && pWeapon)
+	{
+		SDK::Output("IncrementAmmo", std::format("{}\n\t{}, {}\n\t{}, {}",
+			pWeapon->m_iClip1(),
+			TIME_TO_TICKS(pLocal->m_flNextAttack() - I::GlobalVars->curtime),
+			pLocal->m_flNextAttack() - I::GlobalVars->curtime,
+			TIME_TO_TICKS(I::GlobalVars->curtime - pWeapon->m_flReloadPriorNextFire()),
+			I::GlobalVars->curtime - pWeapon->m_flReloadPriorNextFire()).c_str());
+	}
+
+	CALL_ORIGINAL(rcx);
 }
